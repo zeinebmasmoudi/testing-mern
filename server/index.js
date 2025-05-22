8e234d950d315ecbdd4f0c8f162715df890d7b2a
zeinebmasmoudi <106337100+zeinebmasmoudi@users.noreply.github.com>
2025-05-22 11:27:14 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const connectDB = require('./config/db');
 const apiRoutes = require('./routes/api');
 
 const app = express();
-const PORT = process.env.PORT || 5000;  
+const PORT = process.env.PORT || 5005;  
 
 // Connect to MongoDB
 connectDB();
